8e9dd257011b544c02aa5af7e3cd7df4bfae9742
Zaldana <alexandrazaldana@gmail.com>
2019-04-27 12:46:41 -0700

    url

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
